0ce735a1e447025d4b67daaa03d83b74315e7c91
Emil Fresk <emil.fresk@gmail.com>
2017-09-11 14:55:55 +0200

    Added singleton helper

---
+++ helpers/singleton.hpp
@@ -0,0 +1,33 @@
+//          Copyright Emil Fresk 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+namespace esl
+{
+namespace details
+{
+
+// Singleton generator
+template <class C>
+struct singleton
+{
+  static C& instance()
+  {
+    static C inst;
+    return inst;
+  }
+};
+
+} // namespace details
+
+// Singleton helper
+template <class C>
+constexpr inline C& get_singleton()
+{
+  return detail::singleton<C>::instance();
+}
+
+}  // namespace esl
