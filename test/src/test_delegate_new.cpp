8ca1f370c1954d89239f85c422353e36bee74e15
Emil Fresk <emil.fresk@gmail.com>
2017-12-26 21:14:31 +0100

    Reenabled all tests of delegate

--- test/src/test_delegate_new.cpp
+++ test/src/test_delegate_new.cpp
@@ -29,87 +29,87 @@ TEST(test_delegate_new, function_test)
 {
   int i  = 9;
   auto d = callback::from< foo >();
-  //auto d2 = callback::from(foo);
+  auto d2 = callback::from(foo);
 
   ASSERT_EQ(25, d(5));
   ASSERT_EQ(100, d(10));
   ASSERT_EQ(81, d(i));
 
-  //ASSERT_EQ(25, d2(5));
-  //ASSERT_EQ(100, d2(10));
-  //ASSERT_EQ(81, d2(i));
+  ASSERT_EQ(25, d2(5));
+  ASSERT_EQ(100, d2(10));
+  ASSERT_EQ(81, d2(i));
 }
 
-//TEST(test_delegate_new, method_test)
-//{
-//  bar a;
-//
-//  int i  = 9;
-//  auto d = callback::from< bar, &bar::foo >(a);
-//  auto d2 = callback::from(a, &bar::foo);
-//
-//  ASSERT_EQ(25, d(5));
-//  ASSERT_EQ(100, d(10));
-//  ASSERT_EQ(81, d(i));
-//
-//  ASSERT_EQ(25, d2(5));
-//  ASSERT_EQ(100, d2(10));
-//  ASSERT_EQ(81, d2(i));
-//}
-//
-//TEST(test_delegate_new, const_method_test)
-//{
-//  int i = 9;
-//  bar a;
-//
-//  auto d = callback::from< bar, &bar::const_foo >(a);
-//  auto d2 = callback::from(a, &bar::const_foo);
-//
-//  ASSERT_EQ(25, d(5));
-//  ASSERT_EQ(100, d(10));
-//  ASSERT_EQ(81, d(i));
-//
-//  ASSERT_EQ(25, d2(5));
-//  ASSERT_EQ(100, d2(10));
-//  ASSERT_EQ(81, d2(i));
-//}
-//
-//void callback_test(const callback& cb)
-//{
-//  int i = 9;
-//
-//  ASSERT_EQ(25, cb(5));
-//  ASSERT_EQ(100, cb(10));
-//  ASSERT_EQ(81, cb(i));
-//}
-//
-//TEST(test_delegate_new, const_ref_test)
-//{
-//  bar a;
-//
-//  auto d = callback::from< bar, &bar::const_foo >(a);
-//  auto d2 = callback::from(a, &bar::const_foo);
-//
-//  callback_test(d);
-//  callback_test(d2);
-//}
-//
-//TEST(test_delegate_new, comparison_test)
-//{
-//  bar a;
-//
-//  auto d = callback::from< bar, &bar::const_foo >(a);
-//  auto d2 = callback::from(a, &bar::const_foo);
-//
-//  ASSERT_EQ(true, d == d);
-//  ASSERT_EQ(false, d != d);
-//
-//  ASSERT_EQ(true, d2 == d2);
-//  ASSERT_EQ(false, d2 != d2);
-//
-//  ASSERT_EQ(false, d == d2);
-//  ASSERT_EQ(true, d != d2);
-//}
+TEST(test_delegate_new, method_test)
+{
+  bar a;
+
+  int i  = 9;
+  auto d = callback::from< bar, &bar::foo >(a);
+  auto d2 = callback::from(a, &bar::foo);
+
+  ASSERT_EQ(25, d(5));
+  ASSERT_EQ(100, d(10));
+  ASSERT_EQ(81, d(i));
+
+  ASSERT_EQ(25, d2(5));
+  ASSERT_EQ(100, d2(10));
+  ASSERT_EQ(81, d2(i));
+}
+
+TEST(test_delegate_new, const_method_test)
+{
+  int i = 9;
+  bar a;
+
+  auto d = callback::from< bar, &bar::const_foo >(a);
+  auto d2 = callback::from(a, &bar::const_foo);
+
+  ASSERT_EQ(25, d(5));
+  ASSERT_EQ(100, d(10));
+  ASSERT_EQ(81, d(i));
+
+  ASSERT_EQ(25, d2(5));
+  ASSERT_EQ(100, d2(10));
+  ASSERT_EQ(81, d2(i));
+}
+
+void callback_test(const callback& cb)
+{
+  int i = 9;
+
+  ASSERT_EQ(25, cb(5));
+  ASSERT_EQ(100, cb(10));
+  ASSERT_EQ(81, cb(i));
+}
+
+TEST(test_delegate_new, const_ref_test)
+{
+  bar a;
+
+  auto d = callback::from< bar, &bar::const_foo >(a);
+  auto d2 = callback::from(a, &bar::const_foo);
+
+  callback_test(d);
+  callback_test(d2);
+}
+
+TEST(test_delegate_new, comparison_test)
+{
+  bar a;
+
+  auto d = callback::from< bar, &bar::const_foo >(a);
+  auto d2 = callback::from(a, &bar::const_foo);
+
+  ASSERT_EQ(true, d == d);
+  ASSERT_EQ(false, d != d);
+
+  ASSERT_EQ(true, d2 == d2);
+  ASSERT_EQ(false, d2 != d2);
+
+  ASSERT_EQ(false, d == d2);
+  ASSERT_EQ(true, d != d2);
+}
 
 int main(int argc, char *argv[])
 {
