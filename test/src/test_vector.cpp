b974fb750577a5ded3a3524935c7b44b84a17ce6
Emil Fresk <emil.fresk@gmail.com>
2017-12-30 10:50:53 +0100

    Fixed 'smaller vector' test

--- test/src/test_vector.cpp
+++ test/src/test_vector.cpp
@@ -128,18 +128,18 @@ TEST(test_vector, test_make_from_vector)
 
 TEST(test_vector, test_make_from_smaller_vector)
 {
-  esl::vector3i vec1{1, 2, 3};
+  esl::vector2i vec1{1, 2};
 
   esl::vector3i vec2{vec1};
   esl::vector3i vec3 = vec1;
 
   ASSERT_EQ(1, vec2[0]);
   ASSERT_EQ(2, vec2[1]);
-  ASSERT_EQ(3, vec2[2]);
+  ASSERT_EQ(0, vec2[2]);
 
   ASSERT_EQ(1, vec3[0]);
   ASSERT_EQ(2, vec3[1]);
-  ASSERT_EQ(3, vec3[2]);
+  ASSERT_EQ(0, vec3[2]);
 }
 
 TEST(test_vector, test_access)
