b60c15abf4964169abb7d50f75b4a5303a573788
Emil Fresk <emil.fresk@gmail.com>
2018-01-31 13:38:14 +0100

    Fixed scripts

--- scripts/make_gtest_circleci.sh
+++ scripts/make_gtest_circleci.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+##          Copyright Emil Fresk 2017-2018.
+## Distributed under the Boost Software License, Version 1.0.
+##    (See accompanying file LICENSE.md or copy at
+##          http://www.boost.org/LICENSE_1_0.txt)
+
 cd /usr/src/gtest
 cmake . -DBUILD_SHARED_LIBS=ON
 make
--- scripts/style_checker.sh
+++ scripts/style_checker.sh
@@ -6,15 +6,15 @@
 
 bash ./scripts/run_clangformat.sh
 if [[ -n $(git diff --name-status) ]]; then
-  echo "Run clang-format before submitting code."
-  echo "The script is available under ./scripts/run_clangformat.sh"
-  echo ""
-  echo "Files not conforming to style:"
-  echo ""
-  git --no-pager diff --name-status
-  echo ""
-  echo "Git diff:"
-  git --no-pager diff
-  exit -1
+    echo "Run clang-format before submitting code.":
+    echo "The script is available under ./scripts/run_clangformat.sh"
+    echo ""
+    echo "Files not conforming to style:"
+    echo ""
+    git --no-pager diff --name-status
+    echo ""
+    echo "Git diff:"
+    git --no-pager diff
+    exit -1
 fi
 echo "Style checks passed!"
